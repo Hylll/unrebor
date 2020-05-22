1b22902d4031fed8fcc42bd2874a6888c1536b29
Emmanuel Micard <emmanuel.micard@gmail.com>
2020-05-22 18:27:14 +0200

    Setup some assets and plugins

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1 class="home__title">Unrebor</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+  };
+</script>
+
+<style lang="scss" scoped>
+  .home {
+  }
+</style>
