97107888a32a43e08b1b7a0d45f87f52a65a3d80
Nathaël Arki <nathael.arki@golem.ai>
2020-06-03 21:40:27 +0200

    :building_construction: Added basic Symfony

---
+++ symfony/src/Controller/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test", methods={"GET"}, name="test")
+     */
+    public function test(Request $request) {
+        return $this->json('test OK');
+    }
+}
